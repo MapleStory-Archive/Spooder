8231ec46ed19c9625f275e4160c03f32b115c90e
iRiceCrispy <huangeric.j@gmail.com>
2021-10-03 21:36:57 -0700

    Added party command

--- src/util/database/models/Guild.js
+++ src/util/database/models/Guild.js
@@ -2,9 +2,15 @@ module.exports = (mongoose) => {
     const guildSchema = new mongoose.Schema({
         id: { type: String, unique: true },
         name: String,
+        parties: [String],
     }, {
         versionKey: false,
     });
+
+    guildSchema.methods.verifyParty = function(roleId) {
+        const parties = this.parties;
+        return parties.includes(roleId);
+    };
 
     return mongoose.model('Guild', guildSchema);
 };
